0af67570ea5ff21b1fc16862b348fb12939ac474
Catalina <radioncatalina@gmail.com>
2024-01-23 00:06:22 +0200

    first commit

---
+++ src/main/java/org/store/storehub/StoreHubApplication.java
@@ -0,0 +1,13 @@
+package org.store.storehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/store/storehub/StoreHubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.store.storehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
